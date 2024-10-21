89242b1e17788a359e5cc2e11c5820df5d63f8a5
yseek <yseek@naver.com>
2024-10-21 14:09:59 +0900

    BinaryGap commit

---
+++ src/problems/BinaryGap.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class BinaryGap {
+
+    public int solution(int N) {
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        BinaryGap binaryGap = new BinaryGap();
+        int gaps = binaryGap.solution(32);
+
+        System.out.println("gaps = " + gaps);
+    }
+}
