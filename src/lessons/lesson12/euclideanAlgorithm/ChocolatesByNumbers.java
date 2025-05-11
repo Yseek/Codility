6d719f71b526375912129cc4acffd4bc99d7d7cd
YUN <yun5003@naver.com>
2025-05-11 12:04:25 +0000

    ✨ ChocolatesByNumbers 작성중

---
+++ src/lessons/lesson12/euclideanAlgorithm/ChocolatesByNumbers.java
@@ -0,0 +1,17 @@
+package lessons.lesson12.euclideanAlgorithm;
+
+public class ChocolatesByNumbers {
+
+    public int solution(int N, int M) {
+        
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int N = 10;
+        int M = 4;
+        int solution = new ChocolatesByNumbers().solution(N, M);
+        System.out.println("solution = " + solution);
+    }
+}
